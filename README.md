4f0e0478bc14712f7b305764f0f362888774fd11
Osayi Lois <osayilois18@gmail.com>
2025-09-19 23:14:44 +0200

    Primo commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SIW Catalogo
+Spring Boot + PostgreSQL.  
+Avvio: `./mvnw spring-boot:run` (porta 8081).
---
+++ src/test/java/com/osayi/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osayi.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
